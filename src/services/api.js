c56f1239475adac88194f90abb961ce6d1080bb8
Gagan202005 <gagansinghal2005@gmail.com>
2026-01-28 03:48:07 +0530

    remove warnings

--- src/services/api.js
+++ src/services/api.js
@@ -7,8 +7,6 @@ export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
   SIGNUP_API: BASE_URL + "/auth/signup",
   LOGIN_API: BASE_URL + "/auth/login",
-  RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
-  RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
   CHANGE_PASSWORD_API : BASE_URL + "/auth/change-password",
   CONTACT_US_API: BASE_URL + "/auth/contact",
   GEMINI_CHAT_API : BASE_URL + "/auth/chat",
